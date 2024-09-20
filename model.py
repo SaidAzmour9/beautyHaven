c479e26fc3cfdd751f143074e10b243a116d0476
Said Azmour <88777823+SaidAzmour9@users.noreply.github.com>
2024-09-20 17:46:28 +0100

    Update model.py

--- model.py
+++ model.py
@@ -4,28 +4,20 @@ from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
 
-# Load the dataset
 data = pd.read_csv('cosmetics.csv')
 
-# Determine the skin type based on the max value of each skin type column
 data['SkinType'] = data[['Combination', 'Dry', 'Normal', 'Oily', 'Sensitive']].idxmax(axis=1)
 
-# Define features and target
-features = data[['Price', 'Rank']]  # Ensure these are the features used
+features = data[['Price', 'Rank']] 
 target = data['SkinType']
 
-# Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
-# Train the model
+X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 model = DecisionTreeClassifier(random_state=42)
 model.fit(X_train, y_train)
-
-# Make predictions and calculate accuracy
 y_pred = model.predict(X_test)
 accuracy = accuracy_score(y_test, y_pred)
 print(f'Model accuracy: {accuracy}')
 
-# Save the model
 with open('model.pkl', 'wb') as file:
     pickle.dump(model, file)
